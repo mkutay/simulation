b0ed95656c231de11c699238eecbfbf35964004f
mkutay <me@mkutay.dev>
2025-01-31 16:32:57 +0000

    fixed Data.java unused imports

--- src/genetics/Data.java
+++ src/genetics/Data.java
@@ -1,12 +1,6 @@
 package genetics;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-
 import main.Parser;
-import entities.Predator;
 
 public class Data {
   public static final String PATH = "/Users/kutay/code/we-get-these-100s"; // Change this to the path of the project -- this is temporary
