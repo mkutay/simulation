ffde91b1423ac23ecf0d4e444a908190ed2d76b2
mkutay <me@mkutay.dev>
2025-02-18 12:16:07 +0300

    we won't be needing these anymore

--- oldsrc/Location.java
+++
@@ -1,8 +0,0 @@
-package oldsrc;
-/**
- * Represent a location in a rectangular grid.
- *
- * @author David J. Barnes and Michael Kölling
- * @version 7.0
- */
-public record Location(int row, int col) { }
