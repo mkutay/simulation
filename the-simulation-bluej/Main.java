527508064727a553667ef0074038fadf7277dc4e
mkutay <me@mkutay.dev>
2025-02-18 18:30:53 +0300

    changed the bluej project into a zip

--- the-simulation-bluej/Main.java
+++
@@ -1,9 +0,0 @@
-import view.Engine;
-
-public class Main {
-  public static void main(String[] args) {
-    int fps = 60;
-    Engine engine = new Engine(600, 600, fps);
-    engine.start();
-  }
-}
