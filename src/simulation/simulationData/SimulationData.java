3221585f241b711fe4f3f90a6bc39fc34062810f
mkutay <me@mkutay.dev>
2025-02-19 12:16:18 +0300

    improved the document code quality considerations needs to be written asap

--- src/simulation/simulationData/SimulationData.java
+++ src/simulation/simulationData/SimulationData.java
@@ -12,14 +12,14 @@ public class SimulationData {
 
   public double mutationFactor; // The ratio that the genetics will mutate by
   public double entityAgeRate; // Controls how fast the entities age
-  public double fieldScaleFactor;
-  public boolean doDayNightCycle;
-  public boolean doWeatherCycle;
-  public double weatherChangeProbability;
-  public double windStrength;
-  public double stormMovementSpeedFactor;
-  public boolean showQuadTrees;
-  public double dayNightCycleSpeed;
+  public double fieldScaleFactor; // The size of the field, smaller value means more zoomed in.
+  public double weatherChangeProbability; // The probability of the weather changing
+  public double windStrength; // The strength of the wind in windy conditions
+  public double stormMovementSpeedFactor; // When a storm happens, the factor that hinders the entities' speed
+  public double dayNightCycleSpeed; // The speed of the day-night cycle -- how fast the time passes
+  public boolean doDayNightCycle; // Whether the day-night cycle is enabled
+  public boolean doWeatherCycle; // Whether the weather cycle is enabled
+  public boolean showQuadTrees; // Whether the quad trees are shown
 
   public double animalHungerThreshold; // Animals will look for food at this threshold.
   public double animalDyingOfHungerThreshold; // Animals will prioritise looking for food at this threshold.
