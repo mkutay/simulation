2886c0b50cbcde1cd31553692f2e5b158346dd6b
mkutay <me@mkutay.dev>
2025-02-25 13:38:07 +0000

    chore: remove unused next.js project

--- front/src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { getData } from '@/lib/database';
-import { DisplayData } from '@/lib/schema';
-
-export async function GET(request: Request) {
-  const result: DisplayData = await getData();
-  return Response.json({ data: result });
-}
