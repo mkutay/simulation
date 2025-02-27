2e27067b30358b8254448a37f2f808b0b61515eb
mkutay <me@mkutay.dev>
2025-02-27 16:13:56 +0000

    fix(api): remove unused request parameter

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,7 @@
 import { getData } from '@/lib/database';
 import { DisplayData } from '@/lib/schema';
 
-export async function GET(request: Request) {
+export async function GET() {
   const result: DisplayData = await getData();
   return Response.json({ data: result });
 }
